55baeae260da3c39327163ec84091bdbaf253c71
vegardstikbakke <vegard.stikbakke@gmail.com>
2016-04-01 11:36:22 +0200

    One for loop instead of two hard-coded blocks.

--- script.sh
+++ script.sh
@@ -13,30 +13,21 @@
 # Create directory if it does not exist
 mkdir -p processed
 
-# UiO
-cut -c 5-11,83-87,93-96 data/UIO.txt |
-awk -f utils/scorelines.awk |
-sort -s -n -k 1,1 |
-python utils/6lines.py |
-# awk -f utils/6lines.awk | is also possible!
-tr ' ' ',' |
-awk -F ',' -f utils/gather.awk |
-tr ' ' ',' > tmp.csv
+# Cutting necessary info from files; only thing different between the two.
+cut -c 5-11,83-87,93-96 data/UIO.txt > UIO-tmp.txt
+cut -c 6-12,93-97,113-116 data/NTNU.txt > NTNU-tmp.txt
 
-awk 'NR % 2 == 1' tmp.csv > processed/UiO-ORD.csv
-awk 'NR % 2 == 0' tmp.csv > processed/UiO-FORST.csv
-rm tmp.csv
+# Process data from both universities
+for UNIVERSITY in UiO NTNU; do
+    cat ${UNIVERSITY}-tmp.txt |
+    awk -f utils/scorelines.awk |
+    sort -s -n -k 1,1 |
+    python utils/6lines.py | # or awk -f utils/6lines.awk
+    tr ' ' ',' |
+    awk -F ',' -f utils/gather.awk |
+    tr ' ' ',' > tmp.csv
 
-
-# NTNU
-cut -c 6-12,93-97,113-116 data/NTNU.txt |
-awk -f utils/scorelines.awk |
-sort -s -n -k 1,1 |
-python utils/6lines.py |
-tr ' ' ',' |
-awk -F ',' -f utils/gather.awk |
-tr ' ' ',' > tmp.csv
-
-awk 'NR % 2 == 1' tmp.csv > processed/NTNU-ORD.csv
-awk 'NR % 2 == 0' tmp.csv > processed/NTNU-FORST.csv
-rm tmp.csv
+    awk 'NR % 2 == 1' tmp.csv > processed/${UNIVERSITY}-ORD.csv
+    awk 'NR % 2 == 0' tmp.csv > processed/${UNIVERSITY}-FORST.csv
+    rm tmp.csv ${UNIVERSITY}-tmp.txt
+done
