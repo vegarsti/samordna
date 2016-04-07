d8f4abc5e1061c5e45aa56d19f6b39ac4bff2db5
vegardstikbakke <vegard.stikbakke@gmail.com>
2016-04-07 12:03:38 +0200

    Now gathers all university points in one .csv file.

--- script.sh
+++ script.sh
@@ -17,7 +17,7 @@ for uni in NTNU UIO UIB NHH; do
     echo "" > tmp1-${uni}.txt
 done
 
-# Extract data from complete year files
+# Extract data from complete year files into university files
 for year in 2009 2010 2011 2012 2013 2014 2015; do
     sed -n '/Norges teknisk/,/^$/p' data/${year}.txt >> tmp1-NTNU.txt
     sed -n '/Universitetet i Oslo/,/^$/p' data/${year}.txt >> tmp1-UIO.txt
@@ -25,18 +25,22 @@ for year in 2009 2010 2011 2012 2013 2014 2015; do
     sed -n '/Norges Handelshøyskole/,/^$/p' data/${year}.txt >> tmp1-NHH.txt
 done
 
-# Extract programme IDs and names
+# Extract from university files
 function extract {
-    # $1 = interval 1
-    # $2 = interval 2
-    # $3 = interval 3
-    # $4 = university
-    cut -c "$1" tmp1-"$4".txt > tmp-"$4"-1.txt
-    cut -c "$2" tmp1-"$4".txt |
-    tr ',' ':' > tmp-"$4"-2.txt
-    cut -c "$3" tmp1-"$4".txt |
-    tr -s ' ' > tmp-"$4".txt
-    paste -d ',' tmp-"$4"-1.txt tmp-"$4"-2.txt > tmp-"$4"-studies-IDs.txt
+    int1="$1" # interval 1
+    int2="$2" # interval 2
+    int3="$3" # interval 3
+    uni="$4"  # university
+
+    # Extract IDs and programme names
+    cut -c $int1 tmp1-$uni.txt > tmp-$uni-1.txt
+    cut -c $int2 tmp1-$uni.txt |
+    tr ',' ':' > tmp-$uni-2.txt
+    paste -d ',' tmp-$uni-1.txt tmp-$uni-2.txt > tmp-$uni-studies-IDs.txt
+
+    # Extract IDs and scores
+    cut -c $int3 tmp1-$uni.txt |
+    tr -s ' ' > tmp-$uni.txt
 }
 
 # NTNU
@@ -59,27 +63,49 @@ university=NHH
 interval1="5-10"; interval2="12-63"; interval3="5-11,82-86,92-96"
 extract ${interval1} ${interval2} ${interval3} ${university}
 
-# Create directory if it does not exist
+# Create directories if they don't exist
 mkdir -p processed/ordinary
 mkdir -p processed/first
 
 # Process data from all universities
 for uni in UIO NTNU UIB NHH; do
-    awk -f utils/scorelines.awk tmp-${uni}.txt | # Keep lines with actual scores
+    # Variables (for file names)
+    infile=tmp-$uni.txt
+    tmp1=tmp.csv
+    tmp2=tmp-$uni-ORD.csv
+    tmp3=tmp-$uni-FORST.csv
+    studies=tmp-$uni-studies-IDs.txt
+
+    awk -f utils/scorelines.awk ${infile} | # Keep lines with actual scores
     sort -s -n -k 1,1 |                 # Sort by study ID; keep year order
     python utils/7lines.py |            # Keep those IDs with scores all 7 years
     tr ' ' ',' |                        # Replace spaces with commas
     awk -F ',' -f utils/gather.awk |    # Gather all 7 scores for ID on one line
-    tr ' ' ',' > tmp.csv                # Replace spaces with commas
+    tr ' ' ',' > $tmp1                # Replace spaces with commas
 
     # Split into two separate files, one for ordinary quota, one for the other
-    awk 'NR % 2 == 1' tmp.csv > tmp-${uni}-ORD.csv
-    awk 'NR % 2 == 0' tmp.csv > tmp-${uni}-FORST.csv
+    awk 'NR % 2 == 1' $tmp1 > $tmp2
+    awk 'NR % 2 == 0' $tmp1 > $tmp3
 
     # Add programme names and universities
-    python utils/append_titles.py tmp-${uni}-ORD.csv processed/ordinary/${uni}.csv tmp-${uni}-studies-IDs.txt ${uni}
-    python utils/append_titles.py tmp-${uni}-FORST.csv processed/first/${uni}.csv tmp-${uni}-studies-IDs.txt ${uni}
+    python utils/append_titles.py $tmp2 processed/ordinary/$uni.csv $studies $uni
+    python utils/append_titles.py $tmp3 processed/first/$uni.csv $studies $uni
     #      utils/append_titles.py [file w/o names] [outfile] [file w/names] [uni]
+done
+
+
+# Gather all data into one file
+for type in first ordinary; do
+    # Delete file if it exists
+    if [ -f processed/$type/all.csv ]; then
+        rm processed/$type/all.csv
+    fi
+
+    # Gather
+    files=processed/$type/*
+    for file in $files; do
+        sed '1d' $file >> processed/$type/all.csv
+    done
 done
 
 # Cleanup
